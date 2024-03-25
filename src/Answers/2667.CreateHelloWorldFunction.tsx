4cad565ef334dea300f865648868632611ce789d
Dennis Byberg <dennis.byberg@hotmail.com>
2024-03-25 13:18:34 +0100

    deleted unnessicery comments

--- src/Answers/2667.CreateHelloWorldFunction.tsx
+++ src/Answers/2667.CreateHelloWorldFunction.tsx
@@ -1,11 +1,9 @@
 // Write a function createHelloWorld. It should return a new function that always returns "Hello World".
 
-// My solution...
 function createHelloWorld() {
   return function (...args: any): string {
     return "Hello World";
   };
 }
 
-// Check if its working...
 console.log(createHelloWorld());
