7481467de2eedbda381e923d540118dd05684c8a
Dennis Byberg <dennis.byberg@hotmail.com>
2024-03-25 15:14:48 +0100

    deleted unnessicery imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-// import "./Answers/1_TwoSum/TwoSum";
-// import "./Answers/2667_CreateHelloWorldFunction/CreateHelloWorldFunction";
-import "./Answers/9_PalindromeNumber/isPalindrome";
 import "./App.css";
 
 function App() {
