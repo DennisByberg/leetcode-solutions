0ecf174ba0300d7d1be516958603ddacb0b371e0
Dennis Byberg <dennis.byberg@hotmail.com>
2024-03-25 15:15:04 +0100

    +sv lang + deleted clg

--- src/Answers/9_PalindromeNumber/isPalindrome.ts
+++ src/Answers/9_PalindromeNumber/isPalindrome.ts
@@ -1,8 +1,6 @@
-// English: Given an integer x, return true if x is a palindrome, and false otherwise.
-// Svenska: Givet ett heltal x, returnera true om x är ett palindrom, och false annars.
+// En: Given an integer x, return true if x is a palindrome, and false otherwise.
+// Sv: Givet ett heltal x, returnera true om x är ett palindrom, och false annars.
 export function isPalindrome(x: number): boolean {
   const reversedX = parseInt(x.toString().split("").reverse().join(""));
   return x === reversedX ? true : false;
 }
-
-console.log(isPalindrome(1221));
