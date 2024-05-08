3ecd9162acd051ca027a00fa745dd3f0885151f4
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-09 00:22:00 +0200

    refactoring + fixed structure

--- src/Answers/58_Length_Of_Last_Word/lengthOfLastWord.ts
+++ src/Answers/58_Length_Of_Last_Word/lengthOfLastWord.ts
@@ -1,5 +1,6 @@
 // En: Given a string s consisting of words and spaces, return the length of the last word in the string.
 // Sv: Givet en sträng s bestående av ord och mellanslag, returnera längden på det sista ordet i strängen.
+
 export function lengthOfLastWord(s: string) {
   return s
     .split(" ")
--- src/Answers/9_Palindrome_Number/isPalindrome.ts
+++ src/Answers/9_Palindrome_Number/isPalindrome.ts
@@ -1,5 +1,6 @@
 // En: Given an integer x, return true if x is a palindrome, and false otherwise.
 // Sv: Givet ett heltal x, returnera true om x är ett palindrom, och false annars.
+
 export function isPalindrome(x: number): boolean {
   const reversedX = parseInt(x.toString().split("").reverse().join(""));
   return x === reversedX ? true : false;
