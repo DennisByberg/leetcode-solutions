45e047bf2e5b0e7f41cd860c76676c8309c87bb4
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-13 15:35:20 +0200

    changed correct it text

--- Solutions/217_Contains_Duplicate/containsDuplicate.test.ts
+++ Solutions/217_Contains_Duplicate/containsDuplicate.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import { containsDuplicate } from "./containsDuplicate";
 
 describe(containsDuplicate, () => {
-  it("should return true", () => {
+  it("should return true if any value appears at least twice in the array, and false if every element is distinct", () => {
     expect(containsDuplicate([1, 2, 3, 1])).toBe(true);
     expect(containsDuplicate([1, 2, 3, 4])).toBe(false);
     expect(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2])).toBe(true);
