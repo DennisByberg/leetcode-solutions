ca53fff4ce9a20208e5f7e3b4539cbb50cffa5e0
Dennis Byberg <dennis.byberg@hotmail.com>
2024-05-23 23:57:28 +0200

    updated test

--- Solutions/X_Absolute_Difference_Array/absoluteDifferenceArray.test.ts
+++ Solutions/X_Absolute_Difference_Array/absoluteDifferenceArray.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import { absoluteDifferenceArray } from "./absoluteDifferenceArray";
 
 describe(absoluteDifferenceArray, () => {
-  it("should return [0]", () => {
+  it("should return an array with the absolute differences between consecutive elements", () => {
     expect(absoluteDifferenceArray([3, 7, 1, 9, 2])).toEqual([4, 6, 8, 7]);
   });
 });
